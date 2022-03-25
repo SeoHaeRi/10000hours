187423d1be05d91478f5a33a5622d021b934bb51
SeoHaeRi <jackie100425@naver.com>
2022-03-25 23:58:05 +0900

    변수 이벤트 리스너 밖으로 빼기, 훈련버튼 submit 기능 시도

--- 1만.js
+++ 1만.js
@@ -1,12 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let btn = document.querySelector(".btn-exc");
-    btn.addEventListener("click", () => {
-        let expertName = document.querySelector(".expert-name-inp").value;
-        let practiceTime = document.querySelector(".practice-time-inp").value;
-
+    const btnExc = document.querySelector(".btn-exc");
+    const btnGo = document.querySelector(".btn-go");
+    let expertName;
+    let practiceTime;
+    // 며칠 동안 훈련할지 알려주는 버튼
+    btnExc.addEventListener("click", () => {
+        expertName = document.querySelector(".expert-name-inp").value;
+        practiceTime = document.querySelector(".practice-time-inp").value;
         document.querySelector(".expert-name-result").textContent = expertName;
         document.querySelector(".require-day").textContent = Math.ceil(
             10000 / practiceTime
         );
+    });
+    // 훈련 버튼
+    btnGo.addEventListener("click", () => {
+        /*
+        let form = document.createElement('form');
+        form.action = 'https://google.com/search';
+        form.method = 'GET';
+
+        form.innerHTML = '<input name="q" value="`${}`">';
+
+        // 폼을 제출하려면 반드시 폼이 문서 안에 있어야 합니다.
+        document.body.append(form);
+
+        form.submit();
+        */
+        location.href = "https://www.google.co.kr/search?q=" + expertName;
     });
 });
